22016e5991e6099205eaaa862372816fad98a7cb
Noé Júnior <noedombaxe@gmail.com>
2024-08-08 15:04:32 +0100

    🔥feat: add Validator class for cpf

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cpfRoutes = require('./routes/cpfRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api/cpf', cpfRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
